6a2cfc857481c213f8fd323c9b87954da5b28ce1
Dmitry-ULCH <darmoll@mail.ru>
2023-05-29 02:00:24 +0300

    Маленькая поправка для лучшей читаемости кода

--- bot.py
+++ bot.py
@@ -3,7 +3,10 @@ from telegram.ext import Updater, CommandHandler, MessageHandler, Filters
 import settings
 
 
-logging.basicConfig(filename='bot.log', level=logging.INFO, format='%(asctime)s [%(levelname)s]: %(message)s', datefmt='%d/%m/%Y %H:%M:%S')
+logging.basicConfig(filename='bot.log',
+                    level=logging.INFO,
+                    format='%(asctime)s [%(levelname)s]: %(message)s',
+                    datefmt='%d/%m/%Y %H:%M:%S')
 
 
 def greet_user(update, context):
