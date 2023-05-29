d95134af5aee8b4868467c147685d0fe4b9bb0ae
Dmitry-ULCH <darmoll@mail.ru>
2023-05-29 12:56:33 +0300

    Исправления и доработки

--- bot.py
+++ bot.py
@@ -1,6 +1,5 @@
-import logging
+import bot_settings, logging
 from telegram.ext import Updater, CommandHandler, MessageHandler, Filters
-import settings
 
 
 logging.basicConfig(filename='bot.log',
@@ -10,19 +9,16 @@ logging.basicConfig(filename='bot.log',
 
 
 def greet_user(update, context):
-    print('Вызвана команда /start')
     update.message.reply_text('Здравствуй, пользователь! Ты вызвал команду /start')
-    print(update)
 
 
 def talk_to_me(update, context):
     user_text = update.message.text 
-    print(user_text)
     update.message.reply_text(user_text)
 
 
 def main():
-    mybot = Updater(settings.API_KEY, use_context=True)
+    mybot = Updater(bot_settings.API_KEY, use_context=True)
     dp = mybot.dispatcher
     dp.add_handler(CommandHandler('start', greet_user))
     dp.add_handler(MessageHandler(Filters.text, talk_to_me))
@@ -33,8 +29,3 @@ def main():
 
 if __name__ == "__main__":
     main()
-
-"""
-> между блоками без отступа (например, функциями) принято ставить по две пустые строки
-То есть все предыдущие задания хорошо бы тоже переписать в соответсвии с этим и делать так везде?
-"""
